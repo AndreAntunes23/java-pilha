3a90605ff336d8079f2bdc8fbecd68d47250ac67
Andre <andre_lfa@outlook.com>
2021-08-24 16:57:31 -0300

    Aula sobre exceções (exception e error)

--- FluxoComErro.java
+++ FluxoComErro.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComErro {
 
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
@@ -20,9 +20,8 @@ public class Fluxo {
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		throw new MinhaExcecao("deu muito errado");
-
-		//System.out.println("Fim do metodo2");
+		metodo2();
+		System.out.println("Fim do metodo2");
 	}
 
 }
